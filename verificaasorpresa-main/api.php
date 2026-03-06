1a799e69fe45cdb713ba16f7a181defabacf417f
Riccardo Tunisino <tunisinoriccardo@gmail.com>
2026-03-06 09:52:53 +0000

    V2

--- verificaasorpresa-main/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Carica la configurazione dell'app Slim (routing, middleware, servizi).
-require __DIR__ . '/bootstrap/app.php';
-
-// Avvia la gestione delle richieste HTTP.
-$app->run();
--- verificaasorpresa-main/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Include il bootstrap con la definizione completa dell'app.
-require __DIR__ . '/../bootstrap/app.php';
-
-// Esegue l'applicazione Slim.
-$app->run();
